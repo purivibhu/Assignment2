51fd1e5106501bc309a13b474f4f51991fef4d9a
purivibhu <vpuri049@uottawa.ca>
2024-10-02 12:16:23 -0400

    Student Model

---
+++ student/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.TextField(max_length=255)
+    email = models.EmailField()
+    date_of_birth = models.DateField()
+    enrollment_date = models.DateField()
+    grade = models.IntegerField()
--- student_management/settings.py
+++ student_management/settings.py
@@ -30,13 +30,23 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+# INSTALLED_APPS = [
+#     'django.contrib.admin',
+#     'django.contrib.auth',
+#     'django.contrib.contenttypes',
+#     'django.contrib.sessions',
+#     'django.contrib.messages',
+#     'django.contrib.staticfiles',
+# ]
+
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "student.apps.StudentConfig",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
 
 MIDDLEWARE = [
