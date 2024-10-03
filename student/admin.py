0def564ee989d53360d983b2628d2408499284ee
purivibhu <vpuri049@uottawa.ca>
2024-10-03 00:13:09 -0400

    Admin Interface

--- student/admin.py
+++ student/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+# Studen/admin.py
+
+from student.models import Student
+
+class StudentAdmin(admin.ModelAdmin):
+    # pass
+    list_display = ('first_name', 'last_name', 'enrollment_date')
+
+
+admin.site.register(Student, StudentAdmin)
